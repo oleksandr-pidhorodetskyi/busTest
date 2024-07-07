ad57da127322753f989a2c2178f26641f38148a3
APidhorodetskyi <s.pidgorodetsky@gmail.com>
2024-07-08 00:05:44 +0200

    add: google maps

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -4,6 +4,8 @@ import Header from '../components/basic/Header.tsx';
 import {useAppSelector} from '../hooks/useRedux.ts';
 import {selectedBus} from '../store/buses/selectors.ts';
 import SpeedContainer from '../components/screenComponents/SeatsScreen/SpeedContainer.tsx';
+import MapView from 'react-native-maps';
+import {PermissionsAndroid, Platform} from 'react-native';
 
 const MapScreen = () => {
   const selectedBusData = useAppSelector(selectedBus);
@@ -17,7 +19,26 @@ const MapScreen = () => {
           busId: selectedBusData.id,
           busDirection: busDirection,
         }}
-        rightBottomContainer={<SpeedContainer speed="200kmph" />}
+        rightBottomContainer={<SpeedContainer speed="244kmph" />}
+      />
+      <MapView
+        provider={'google'}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsUserLocation
+        showsMyLocationButton
+        style={{width: '100%', height: '50%'}}
+        onMapReady={() => {
+          if (Platform.OS === 'android') {
+            PermissionsAndroid.request(
+              PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            );
+          }
+        }}
       />
     </ScreenContainer>
   );
